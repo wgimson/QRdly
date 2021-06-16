b154b7cf2a57cf1b7cd7c4304330b2b60df87ffb
Alex Gimson <alexgimson@gmail.com>
2021-06-16 09:48:04 -0500

    Homepage (#5)
    * homepage WPA
    
    * wpa
    
    * sdsa
    
    * home page buttons

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
-
+const aboutController = require('./controllers/about');
+const faqController = require('./controllers/faq');
+const termsController = require('./controllers/terms');
 /**
  * API keys and Passport configuration.
  */
@@ -125,6 +127,9 @@ app.use('/webfonts', express.static(path.join(__dirname, 'node_modules/@fortawes
  * Primary app routes.
  */
 app.get('/', homeController.index);
+app.get('/about', aboutController.index);
+app.get('/faq', faqController.index);
+app.get('/terms', termsController.index);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
@@ -143,7 +148,6 @@ app.post('/account/profile', passportConfig.isAuthenticated, userController.post
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-
 /**
  * API examples routes.
  */
