c5fed8b12bdeb44fa5b031985ac30305699337ec
William Gimson <wgimson@gmail.com>
2021-06-21 01:07:15 -0400

    fix: scripts must be loaded in layout pug view

--- public/js/customerWaitingList/customerWaitingList.js
+++ public/js/customerWaitingList/customerWaitingList.js
@@ -1,9 +1,9 @@
 /* eslint-env jquery, browser */
-$(() => {
-  function editCustomer() {
-    const thiss = $(this);
-    console.log(`lets edit: ${thiss.attr('data-id')}`);
-  }
+// $(() => {
+//   // function editCustomer() {
+//   //   const thiss = $(this);
+//   //   console.log(`lets edit: ${thiss.attr('data-id')}`);
+//   // }
 
-  $('.btn-edit-customer').on('click', editCustomer);
-});
+//   // $('.btn-edit-customer').on('click', editCustomer);
+// });
