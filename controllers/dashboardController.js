d7ac227eec9e8f256ecd9ff7e0178a372ed4d342
William Gimson <wgimson@gmail.com>
2021-07-27 23:23:10 -0400

    first business card pulled from db and displayed

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,10 +1,22 @@
+const BusinessCard = require('../models/BusinessCard');
+
 /**
  * GET /dashboard
  * Admin users Dashboard
  */
 exports.getDashboard = (req, res) => {
-  res.render('dashboard/dashboard/dashboard', {
-    title: 'Admin Dashboard',
-    user: req.user
+  BusinessCard.find({}, (err, allBusinessCards) => {
+    if (!err) {
+      console.log('UserId:');
+      const firstBusinessCard = allBusinessCards[0];
+      res.render('dashboard/dashboard/dashboard', {
+        title: 'Admin Dashboard',
+        user: req.user,
+        businessCard: firstBusinessCard
+      });
+    } else {
+      console.log('error');
+      throw err;
+    }
   });
 };
