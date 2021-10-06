c64077fda6c2bad7fd3e7aa8d573c8b2a7c6565c
alexgimson86 <alexgimson@gmail.com>
2021-10-06 17:40:29 -0500

    changed to using req.user object instead of getting it from mycard

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -4,8 +4,8 @@ const BusinessCard = require('../models/BusinessCard');
 exports.getAdminCalendar = async (req, res) => {
   const myCard = await BusinessCard.findOne( { userId: req.user.id } ).exec()
   const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-  let sortedMeetings = await Meeting.find( { adminId: { $eq: myCard.userId } } ).sort( { date: 1} )
-  var lastMeeting = await Meeting.find( { adminId: { $eq: myCard.userId } } ).limit(1).sort( { $natural:-1 } ).exec()
+  let sortedMeetings = await Meeting.find( { adminId: { $eq: req.user.id } } ).sort( { date: 1} )
+  var lastMeeting = await Meeting.find( { adminId: { $eq: req.user.id } } ).limit(1).sort( { $natural:-1 } ).exec()
   res.render('dashboard/adminCalendar', {
     title: 'Admin Calendar',
     user: req.user,
