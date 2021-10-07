7dce6daff574a8971efefa2ef8005637fc2af44d
alexgimson86 <alexgimson@gmail.com>
2021-10-07 12:16:21 -0500

    forgot this file

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -2,30 +2,37 @@ const Meeting = require('../models/Meeting');
 const BusinessCard = require('../models/BusinessCard');
 
 exports.getAdminCalendar = async (req, res) => {
-  const myCard = await BusinessCard.findOne( { userId: req.user.id } ).exec()
+  
+  var company = "";
+  await BusinessCard.findOne( { userId: req.user.id }, (err, card) => {
+    if(card){
+      company = card.companyName
+    }
+  })
   const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
   let sortedMeetings = await Meeting.find( { adminId: { $eq: req.user.id } } ).sort( { date: 1} )
-  var lastMeeting = await Meeting.find( { adminId: { $eq: req.user.id } } ).limit(1).sort( { $natural:-1 } ).exec()
   res.render('dashboard/adminCalendar', {
     title: 'Admin Calendar',
     user: req.user,
-    businessName: myCard.companyName,
+    businessName: company,
     date: date,
     meetings: sortedMeetings,
-    lastMeeting: lastMeeting[0]
   });
 };
 exports.getFrontEndCalendar  =  async (req, res) => {
   //give id 
-  const myCard = await BusinessCard.findOne( { userId: req.params.id } ).exec()
+  var company = "";
+  await BusinessCard.findOne( { userId: req.params.id }, (err, card) => {
+    if(card){
+      company = card.companyName
+    }
+  })
   const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-  let sortedMeetings = await Meeting.find( { adminId: { $eq: myCard.userId } } ).sort( { date: 1} )
-  var lastMeeting = await Meeting.find( { adminId: { $eq: myCard.userId } } ).limit(1).sort( { $natural:-1 } ).exec()
+  let sortedMeetings = await Meeting.find( { adminId: { $eq: req.params.id } } ).sort( { date: 1} )
   res.render('ui/frontEndCalendar', {
     title: 'Front End Calendar',
-    businessName: myCard.companyName,
+    businessName: company,
     date: date,
     meetings: sortedMeetings,
-    lastMeeting: lastMeeting[0]
   });
 };
