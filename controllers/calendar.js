023bbd010283edc86e07cd213bf1891919abddac
William Gimson <wgimson@gmail.com>
2022-04-17 03:38:52 -0400

    new user can register with email and pass (#58)

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -2,37 +2,36 @@ const Meeting = require('../models/Meeting');
 const BusinessCard = require('../models/BusinessCard');
 
 exports.getAdminCalendar = async (req, res) => {
-  
-  var company = "";
-  await BusinessCard.findOne( { userId: req.user.id }, (err, card) => {
-    if(card){
-      company = card.companyName
+  let company = '';
+  await BusinessCard.findOne({ userId: req.user.id }, (err, card) => {
+    if (card) {
+      company = card.companyName;
     }
-  })
-  const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-  let sortedMeetings = await Meeting.find( { adminId: { $eq: req.user.id } } ).sort( { date: 1} )
+  });
+  const date = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+  const sortedMeetings = await Meeting.find({ adminId: { $eq: req.user.id } }).sort({ date: 1 });
   res.render('dashboard/adminCalendar', {
     title: 'Admin Calendar',
     user: req.user,
     businessName: company,
-    date: date,
+    date,
     meetings: sortedMeetings,
   });
 };
-exports.getFrontEndCalendar  =  async (req, res) => {
-  //give id 
-  var company = "";
-  await BusinessCard.findOne( { userId: req.params.id }, (err, card) => {
-    if(card){
-      company = card.companyName
+exports.getFrontEndCalendar = async (req, res) => {
+  // give id
+  let company = '';
+  await BusinessCard.findOne({ userId: req.params.id }, (err, card) => {
+    if (card) {
+      company = card.companyName;
     }
-  })
-  const date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-  let sortedMeetings = await Meeting.find( { adminId: { $eq: req.params.id } } ).sort( { date: 1} )
+  });
+  const date = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+  const sortedMeetings = await Meeting.find({ adminId: { $eq: req.params.id } }).sort({ date: 1 });
   res.render('ui/frontEndCalendar', {
     title: 'Front End Calendar',
     businessName: company,
-    date: date,
+    date,
     meetings: sortedMeetings,
     adminId: req.params.id,
   });
---
+++ models/RegisteredUser.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const registeredUserSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  emailVerificationToken: String,
+  emailVerified: Boolean,
+  isAdmin: { type: Boolean, default: false },
+
+}, { timestamps: true });
+
+/**
+ * Password hash middleware.
+ */
+registeredUserSchema.pre('save', function save(next) {
+  const registeredUserSchema = this;
+  if (!registeredUserSchema.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(registeredUserSchema.password, salt, (err, hash) => {
+      if (err) { return next(err); }
+      registeredUserSchema.password = hash;
+      next();
+    });
+  });
+});
+
+/**
+ * Helper method for validating user's password.
+ */
+registeredUserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+const RegisteredUser = mongoose.model('RegisteredUser', registeredUserSchema);
+
+module.exports = RegisteredUser;
